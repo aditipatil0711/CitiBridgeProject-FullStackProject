7081156909b5c98aad4c41f6ff176de42bc3a0df
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2020-07-28 12:47:59 +0530

    Add files via upload

--- frontend/angular-citiproject/src/app/services/watchlist.service.ts
+++ frontend/angular-citiproject/src/app/services/watchlist.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { WatchlistItem } from '../common/watchlist-item';
 import { Subject } from 'rxjs';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
+import {map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,87 +12,107 @@ export class WatchlistService {
 
   watchlistItems: WatchlistItem[] = [];
 
+
   totalPrice: Subject<number> = new Subject<number>();
   totalQuantity: Subject<number> = new Subject<number>();
 
-  constructor() { }
+  private watchlistUrl = "http://localhost:8080/api/watchlist-details";
+  constructor(private httpClient: HttpClient) { }
 
-  addToWatchlist(theWatchlistItem:WatchlistItem){
+
+  addToWatchlist(theWatchlistItem: WatchlistItem) {
 
     //check if we already have the stock in our watchlist
-    let alreadyExistsInWatchlist:boolean = false;
+    let alreadyExistsInWatchlist: boolean = false;
     let existingWatchlistStock: WatchlistItem = undefined;
 
-    if(this.watchlistItems.length > 0){
-    //find the item in the cart based on stockId
-      
-      existingWatchlistStock = this.watchlistItems.find(tempWatchlistItem => tempWatchlistItem.id === theWatchlistItem.id)
-      
-    //check if we found it
-    alreadyExistsInWatchlist = (existingWatchlistStock != undefined);
-  }
+    if (this.watchlistItems.length > 0) {
+      //find the item in the cart based on stockId
 
-  if(alreadyExistsInWatchlist){
-    existingWatchlistStock.quantity++;
+      existingWatchlistStock = this.watchlistItems.find(
+        tempWatchlistItem => tempWatchlistItem.id === theWatchlistItem.id)
 
-  }
-  else{
-    this.watchlistItems.push(theWatchlistItem);
+      //check if we found it
+      alreadyExistsInWatchlist = (existingWatchlistStock != undefined);
+    }
 
-  }
-  //compute cart total price and total quantity
-  this.computeWatchlistTotals();
-  
-  }
+    if (alreadyExistsInWatchlist) {
+      existingWatchlistStock.quantity++;
 
-    computeWatchlistTotals() {
+    }
+    else {
+      this.watchlistItems.push(theWatchlistItem);
+
+    }
+    //compute cart total price and total quantity
+    this.computeWatchlistTotals();
+
+     }
+
+  computeWatchlistTotals() {
     let totalPriceValue: number = 0;
     let totalQuantityValue: number = 0;
 
-    for(let currentWatchlistItem of this.watchlistItems){
-      totalPriceValue += currentWatchlistItem.quantity * currentWatchlistItem.currentPrice;
+    for (let currentWatchlistItem of this.watchlistItems) {
+      totalPriceValue += currentWatchlistItem.quantity * currentWatchlistItem.savedPrice;
       totalQuantityValue += currentWatchlistItem.quantity;
     }
 
     //publish the new values .... all subscroibers will recieve the new data
-      this.totalPrice.next(totalPriceValue);
-      this.totalQuantity.next(totalQuantityValue);
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
 
-      //log cart data just for debugging purposes
-      this.logWatchlistData(totalPriceValue,totalQuantityValue);
+    //log cart data just for debugging purposes
+    this.logWatchlistData(totalPriceValue, totalQuantityValue);
 
-  
   }
+
   logWatchlistData(totalPriceValue: number, totalQuantityValue: number) {
     console.log("Contents of the Watchlist");
-    for(let tempWatchlistItem of this.watchlistItems){
-      const subTotalPrice = tempWatchlistItem.quantity *tempWatchlistItem.currentPrice;
-      console.log(`name: ${tempWatchlistItem.name},quantity: ${tempWatchlistItem.quantity}, currentPrice = ${tempWatchlistItem}, subTotalPrice: ${subTotalPrice}`);
+    this.getWatchlist();
+    console.log(`details: ${this.watchlistItems}`)
+    
+    for (let tempWatchlistItem of this.watchlistItems) {
+      const subTotalPrice = tempWatchlistItem.quantity * tempWatchlistItem.savedPrice;
+      console.log(`name: ${tempWatchlistItem.stockName},quantity: ${tempWatchlistItem.quantity}, currentPrice = ${tempWatchlistItem.savedPrice}, subTotalPrice: ${subTotalPrice}`);
     }
 
     console.log(`totalPrice:${totalPriceValue.toFixed(2)}, totalQuantity:${totalQuantityValue}`);
     console.log("----------------");
 
-    
+
   }
 
   decrementQuantity(theWatchlistItem: WatchlistItem) {
     theWatchlistItem.quantity--;
-    if(theWatchlistItem.quantity === 0){
+    if (theWatchlistItem.quantity === 0) {
       this.remove(theWatchlistItem);
     }
-    else{
+    else {
       this.computeWatchlistTotals();
     }
 
   }
   remove(theWatchlistItem: WatchlistItem) {
-      // get index of item in array
-      const itemIndex = this.watchlistItems.findIndex(tempWatchlistItem => tempWatchlistItem.id === theWatchlistItem.id);
-      //if found remove the item from the array
-      if(itemIndex > -1){
-        this.watchlistItems.splice(itemIndex,1);
-        this.computeWatchlistTotals();
-      }
+    // get index of item in array
+    const itemIndex = this.watchlistItems.findIndex(tempWatchlistItem => tempWatchlistItem.id === theWatchlistItem.id);
+    //if found remove the item from the array
+    if (itemIndex > -1) {
+      this.watchlistItems.splice(itemIndex, 1);
+      this.computeWatchlistTotals();
+    }
+  }
+
+  getWatchlist(): Observable<WatchlistItem[]> {
+    return this.httpClient.get<GetResponseWatchlistItems>(this.watchlistUrl).pipe(
+      map(response => response._embedded.watchlistDetails)
+    );
+  }
+
+}
+
+interface GetResponseWatchlistItems {
+  _embedded: {
+    watchlistDetails: WatchlistItem[];
   }
 }
